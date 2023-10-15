87d003126501b20ef93d27222b2e3559de65aa4e
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-10-15 21:23:32 +0300

    config routes cars
    add commit for routes
    add route getCarById

--- routes/api/cars.js
+++ routes/api/cars.js
@@ -6,15 +6,22 @@ const ctrl = require("../../controllers/cars");
 
 const router = express.Router();
 
+// !======== запрос для всех объявлени  машин ===========
 // router.get("/", authenticate, ctrl.getAll);
 router.get("/", ctrl.getAll);
 
+// !======== запрос для одного объявления  машины ===========
 // router.get("/:contactId", authenticate, isValidId, ctrl.getContactById);
+router.get("/:carId", ctrl.getCarById);
 
+// !======== запрос для создания объявления ===========
 // router.post("/", authenticate, ctrl.create);
 
+// !======== запрос для изменения объявления ===========
 // router.put("/:contactId", authenticate, isValidId, ctrl.updateContact);
+// router.put("/:carId", authenticate, isValidId, ctrl.updateContact);
 
+// !======== запрос для добавления объявления в избранное  ===========
 // router.patch(
 //   "/:contactId/favorite",
 //   authenticate,
@@ -22,6 +29,14 @@ router.get("/", ctrl.getAll);
 //   ctrl.updateFavorite
 // );
 
-// router.delete("/:contactId", authenticate, isValidId, ctrl.remove);
+// router.patch(
+//   "/:carId/favorite",
+//   authenticate,
+//   isValidId,
+//   ctrl.updateFavorite
+// );
+
+// !======== запрос для удаления объявления ===========
+// router.delete("/:carId", authenticate, isValidId, ctrl.remove);
 
 module.exports = router;
