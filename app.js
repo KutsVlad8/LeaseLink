87037ec384ea670af4afd77bf9c9aea470b1f346
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-10-16 17:17:57 +0300

    remove dotenv from app

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
-const dotenv = require("dotenv");
 
 const { authRouter, advertsRouter } = require("./routes/api");
-
-dotenv.config();
 
 const app = express();
 
