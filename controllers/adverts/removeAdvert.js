98ce73d27fad7e92be7451bd1dee39c4f4b406c9
Vladislav Kutsevich <kutsevichvlad18@gmail.com>
2023-10-16 20:56:02 +0300

    add controller remove

--- controllers/adverts/removeAdvert.js
+++ controllers/adverts/removeAdvert.js
@@ -3,16 +3,16 @@ const Adverts = require("../../models/adverts");
 const { HttpError } = require("../../helpers");
 
 const removeAdvert = async (req, res) => {
-  const { _id: owner } = req.user;
-  const { contactId } = req.params;
-  const result = await Adverts.findOneAndRemove({ _id: contactId, owner });
+  // const { _id: owner } = req.user;
+  const { advertId } = req.params;
+  const result = await Adverts.findOneAndRemove({ _id: advertId });
 
   if (!result) {
     throw HttpError(404, "Not found");
   }
 
   res.json({
-    message: "contact deleted",
+    message: "Advert deleted",
   });
 };
 
